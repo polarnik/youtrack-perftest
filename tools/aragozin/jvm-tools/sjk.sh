c083400e84943cab591ab9222b7d0e7de2514be2
Vyacheslav Smirnov <owasp@yandex.ru>
2024-09-03 16:06:52 +0400

    Fix JFR port options

--- tools/aragozin/jvm-tools/sjk.sh
+++ tools/aragozin/jvm-tools/sjk.sh
@@ -1,70 +1,62 @@
 #!/bin/bash
 
-mkdir -p $reportFolder
-
-jmeterPID=`pgrep -f "java.*-jar ApacheJMeter"`
-
-while ! [ -n "$jmeterPID" ]
-do
-    sleep 0.1
-    jmeterPID=`pgrep -f "java.*-jar ApacheJMeter"`
-done
+export SJK_VERSION=0.23
 
 dt=`date '+%Y-%m-%d_%H-%M-%S'`
-jmeterScript=`pgrep -af "java.*-jar ApacheJMeter" | awk -F "-t $jmeterScriptFolder" '{ split($2, t, " "); print t[1]}'`
-sampleTraceName="$reportFolder""$jmeterScript.$dt.sdt"
 
-echo "sjk run for $jmeterPID at $dt into $sampleTraceName"
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.8.0:get \
+  -Dartifact=org.gridkit.jvmtool:sjk-plus:${SJK_VERSION}
+
+mvn dependency:copy
+
+ls -la /tmp
+
+
+sampleTraceName="/tmp/${dt}.sdt"
+
+echo "sjk runs at $dt into $sampleTraceName"
+
 java -jar /tmp/sjk-plus-${SJK_VERSION}.jar \
     stcap \
-    --pid $jmeterPID \
+    --socket 127.0.0.1:9010 \
     --timeout 600s \
-    --output "$sampleTraceName"
+    --output "${sampleTraceName}"
 
 echo "complete $sampleTraceName"
 
-java -jar "$jmeterLibFolder"sjk-plus-0.11.jar \
-    ssa \
-    --file "$sampleTraceName" \
-    --thread-name "Thread Group.*" \
-    --histo \
-    > "$sampleTraceName".histo.txt
+declare -A threadGroups
+threadGroups["ErrLoggerFor"]="ErrLoggerFor.+"
+threadGroups["Exodus"]="Exodus.+"
+threadGroups["ForkJoinPool"]="ForkJoinPool.+"
+threadGroups["Log4j2"]="Log4j2.+"
+threadGroups["OutLoggerFor"]="OutLoggerFor.+"
+threadGroups["article-analysis"]="article-analysis-.+"
+threadGroups["issue-analysis"]="issue-analysis-.+"
+threadGroups["local-scheduling"]="local-scheduling-.+"
+threadGroups["Thread"]="Thread-.+"
+threadGroups["qtp"]="qtp.+"
+threadGroups["sending-durability"]="sending-durability-.+"
+threadGroups["text-index"]="text-index-.+"
+threadGroups["user-action"]="user-action-.+"
+threadGroups["workflow-scheduled"]="workflow-scheduled-.+"
+threadGroups["similar-issues"]="similar-issues-.+"
 
-java -jar "$jmeterLibFolder"sjk-plus-0.11.jar \
-    ssa \
-    --file "$sampleTraceName" \
-    --thread-name "Thread Group.*" \
-    --histo \
-    --by-term \
-    > "$sampleTraceName".histo.by-term.txt
 
-java -jar "$jmeterLibFolder"sjk-plus-0.11.jar \
-    ssa \
-    --file "$sampleTraceName" \
-    --thread-name "Thread Group.*" \
-    --thread-info \
-    > "$sampleTraceName".thread-info.txt
+declare -A options
+options["histo.txt"]="--histo"
+options["histo.by-term.txt"]="--histo --by-term"
+options["thread-info.txt"]="--thread-info"
+options["flame.svg"]="--flame --width 3500"
 
-java -jar "$jmeterLibFolder"sjk-plus-0.11.jar \
-    ssa \
-    --file "$sampleTraceName" \
-    --thread-name "Thread Group.*" \
-    --flame \
-    --title "$jmeterScript" \
-    --width 3500 \
-    > "$sampleTraceName".flame.svg
 
-sampleTraceNameSmall="$reportFolder""$jmeterScript.$dt.small.sdt"
-
-java -jar "$jmeterLibFolder"sjk-plus-0.11.jar \
-    stcpy \
-    --input "$sampleTraceName" \
-    --thread-name "Thread Group.*" \
-    --subsample 0.1 \
-    --output $sampleTraceNameSmall
-
-java -jar "$jmeterLibFolder"sjk-plus-0.11.jar \
-    flame \
-    --file "$sampleTraceNameSmall" \
-    --thread-name "Thread Group.*" \
-    --output "$sampleTraceNameSmall".flame.html
+for threadGroup in "${!threadGroups[@]}"; do
+  for opt in "${!options[@]}"; do
+      echo "Work with threadGroup: ${threadGroup} (${threadGroups[$threadGroup]}) and options: ${opt} (${options[$opt]})"
+      java -jar /tmp/sjk-plus-${SJK_VERSION}.jar \
+          ssa \
+          --file "$sampleTraceName" \
+          --thread-name "${threadGroups[$threadGroup]}" \
+          ${options[$opt]} \
+          > "${sampleTraceName}.${threadGroup}.${opt}"
+  done
+done
